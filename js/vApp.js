2c436f4a288e8b08cdaf4d818a2228e39f3b3edd
Yewhral <zabrzyjewski@gmail.com>
2017-07-07 19:35:12 +0200

    Removing tasks fixed

--- js/vApp.js
+++ js/vApp.js
@@ -2,20 +2,21 @@ const vApp = {
 
     addTask: () => {
         const taskText = document.getElementById('newTask');
-        const contentBox = document.getElementById('tasks-holder');
+        const contentBox = document.getElementById('tasksholder');
         const taskCreator = document.createElement('div');
         const deleteButtonCreator = document.createElement('button');
 
-        taskCreator.className ='task-box';
+        taskCreator.className = 'task-box';
         taskCreator.innerHTML = taskText.value;
         contentBox.insertBefore(taskCreator,contentBox.childNodes[0]);
         taskText.value = '';
 
         deleteButtonCreator.innerHTML = 'x';
-        deleteButtonCreator.addEventListener('click', () => {
-            (contentBox.removeChild(this.parentNode));
-        });
         deleteButtonCreator.setAttribute('class', "x-button");
+        deleteButtonCreator.addEventListener('click', function() {
+            contentBox.removeChild(this.parentNode);
+        });
+
 
         taskCreator.appendChild(deleteButtonCreator);
     }
