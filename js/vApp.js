058064d194a0941b0f57637f170af0b076e36e2c
Yewhral <zabrzyjewski@gmail.com>
2017-07-07 17:08:30 +0200

    Added task removing button

--- js/vApp.js
+++ js/vApp.js
@@ -4,14 +4,21 @@ const vApp = {
         const taskText = document.getElementById('newTask');
         const contentBox = document.getElementById('tasks-holder');
         const taskCreator = document.createElement('div');
-        // TODO rename
-        taskCreator.className ='kurczak';
+        const deleteButtonCreator = document.createElement('button');
+
+        taskCreator.className ='task-box';
         taskCreator.innerHTML = taskText.value;
         contentBox.insertBefore(taskCreator,contentBox.childNodes[0]);
         taskText.value = '';
+
+        deleteButtonCreator.innerHTML = 'x';
+        deleteButtonCreator.addEventListener('click', () => {
+            (contentBox.removeChild(this.parentNode));
+        });
+        deleteButtonCreator.setAttribute('class', "x-button");
+
+        taskCreator.appendChild(deleteButtonCreator);
     }
-
-
 
 
 
