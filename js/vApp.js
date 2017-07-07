7a317f4b19b86e7e309277b5b4b583ffd633cabc
Yewhral <zabrzyjewski@gmail.com>
2017-07-07 20:23:41 +0200

    Prepatations fro editing

--- js/vApp.js
+++ js/vApp.js
@@ -4,21 +4,38 @@ const vApp = {
         const taskText = document.getElementById('newTask');
         const contentBox = document.getElementById('tasksholder');
         const taskCreator = document.createElement('div');
-        const deleteButtonCreator = document.createElement('button');
+        const deleteButton = document.createElement('button');
+        const editButton = document.createElement('button');
 
-        taskCreator.className = 'task-box';
-        taskCreator.innerHTML = taskText.value;
-        contentBox.insertBefore(taskCreator,contentBox.childNodes[0]);
-        taskText.value = '';
+        if (taskText.value.length > 0) {        // protection against empty input
+            taskCreator.className = 'task-box';
+            taskCreator.innerHTML = taskText.value;
+            contentBox.insertBefore(taskCreator, contentBox.childNodes[0]);
+            taskText.value = '';
 
-        deleteButtonCreator.innerHTML = 'x';
-        deleteButtonCreator.setAttribute('class', "x-button");
-        deleteButtonCreator.addEventListener('click', function() {
-            contentBox.removeChild(this.parentNode);
-        });
+            deleteButton.style.backgroundImage = "url('img/delete.png')";
+            deleteButton.addEventListener('click', function() {
+                contentBox.removeChild(this.parentNode);
+            });
 
+            editButton.style.backgroundImage = "url('img/edit.png')";
+            editButton.addEventListener('click', function() {
+                //TODO add function to deal with text edition, maybe an input field and replace div with okay button
+            });
 
-        taskCreator.appendChild(deleteButtonCreator);
+            taskCreator.appendChild(editButton);
+            taskCreator.appendChild(deleteButton);
+            taskText.placeholder = 'What else needs to be done?';
+
+        } else {
+            vApp.warn();
+        }
+    },
+
+    warn: () => {
+        const taskText = document.getElementById('newTask');
+        taskText.placeholder = 'You can\'t add an empty task!';
+        return false;
     }
 
 
